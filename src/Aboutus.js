99faa6df00b17cf015421de6f1253535cc2051fd
lichvayne <sandro.mekoshkishvili.1@iliauni.edu.ge>
2022-02-09 16:49:36 +0400

    Update 2.4

--- src/Aboutus.js
+++ src/Aboutus.js
@@ -8,7 +8,9 @@ function Aboutus(){
 
     return( 
    <div className="mt-2">
+       
        <div className='Aboutus AboutusP'>
+          
        <p class="fw-bold">About Us</p>
         <p class="fw-light">Webmedia offers a unique service to all investors and home buyers interested in benefiting from the real estate opportunities in Georgia’s capital, 
         guiding potential buyers through the entire purchasing process from start to finish:</p>
@@ -19,7 +21,9 @@ function Aboutus(){
         <p class="fw-light">We help you to register the property at the Public Registry Office. If you require Permanent Residence in Georgia, we can assist you with the process.</p>
         <p class="fw-bold">Finishing</p>
         <p class="fw-light">Most apartments in Tbilisi are sold on a White Frame basis. Our Construction Division will work with buyers to finish their apartments to their specifications and budget – usually within six weeks.</p>
+         <hr></hr>
         </div>
+        
    </div>
 
     )
